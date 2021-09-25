79d9a9a55785fa12ee8c1f19f26d5f9785b6c083
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2021-09-25 15:29:00 +0200

    Rebase from 'debian/foxy/simple_launch'

--- example/included_launch.py
+++ example/included_launch.py
@@ -11,12 +11,23 @@ def generate_launch_description():
     sl.declare_arg('x', default_value = 0, description='x-offset of the robot')
     sl.declare_arg('y', default_value = 0, description='y-offset of the robot')
     sl.declare_arg('use_gui', default_value = True, description='Use JSP gui')
+    sl.declare_arg('use_xacro_prefixing', default_value = True)
     
     xacro_args = sl.arg_map(('prefix', 'x', 'y'))
     xacro_args['prefix'] = [xacro_args['prefix'], '/']  # cannot just sum a launch argument and a string
     
     with sl.group(ns=sl.arg('prefix')):
-        sl.robot_state_publisher('simple_launch', 'turret.xacro', xacro_args = xacro_args)
+        
+        with sl.group(if_arg='use_xacro_prefixing'):
+            # prefix with xacro, ok if the xacro file was written with prefix in mind
+            sl.robot_state_publisher('simple_launch', 'turret.xacro', xacro_args = xacro_args)
+            sl.abs_node_ns()
+            
+        with sl.group(unless_arg='use_xacro_prefixing'):
+            # or prefix with robot_state_publisher
+            # gz_plugins=True will also prefix Gazebo-published link names and namespace Gazebo-published topics (none for this robot)
+            sl.robot_state_publisher('simple_launch', 'turret.xacro', parameters={'frame_prefix': sl.arg('prefix')}, gz_plugins=True)            
+            
         sl.joint_state_publisher(sources_list = ['source_joints'], use_gui = sl.arg('use_gui'))
         
     return sl.launch_description()
